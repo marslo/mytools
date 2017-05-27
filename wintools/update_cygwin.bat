1e00a323c5ca045e9702ba5a8d0ad8c74ee295d5
Marslo Jiao <marslo.jiao@philips.com>
2017-05-27 20:58:57 +0800

    add script for update cygwin

---
+++ wintools/update_cygwin.bat
@@ -0,0 +1,5 @@
+@echo off
+
+cd C:\Marslo\Tools\Software\Programming\Cygwin
+wget http://cygwin.com/setup-x86_64.exe
+cygwin-setup-x86_64_2.8.0_20170526.exe  -q --upgrade-also q --root C:\Marslo\MyProgramFiles\cygwin64
