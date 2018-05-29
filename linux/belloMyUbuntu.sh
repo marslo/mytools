4f681bd774d4b8b11c7c23d70517ecfc96198656
marslo <marslo.jiao@philips.com>
2018-05-29 18:03:44 +0800

    update script due to shellcheck warning

--- linux/belloMyUbuntu.sh
+++ linux/belloMyUbuntu.sh
@@ -39,8 +39,8 @@ function setupEnv() {
   sudo sed -i -e "s:^\($(whoami).*$\):# \1:" /etc/sudoers
   sudo bash -c "echo \"$(whoami)   ALL=(ALL:ALL) NOPASSWD:ALL\" >> /etc/sudoers"
 
-  sudo cp /etc/bash.bashrc{,.org.${TIMESTAMPE}}
-  sudo cp ${SSHDFILE}{,.org.${TIMESTAMPE}}
+  sudo cp "/etc/bash.bashrc{,.org.${TIMESTAMPE}}"
+  sudo cp "${SSHDFILE}{,.org.${TIMESTAMPE}}"
   sudo chown -R "$(whoami)":admin /usr/local
 
   sudo chown -R "$(whoami)":"$(whoami)" /opt
@@ -97,9 +97,9 @@ set mark-symlinked-directories on
 set print-completions-horizontally on
 EOF
 
-[ ! -d $HOME/.marslo/ss ] && mkdir -p $HOME/.marslo/ss
-if [ ! -f $HOME/.marslo/ss/ssmarslo.json ]; then
-cat > $HOME/.marslo/ss/ssmarslo.json << EOF
+[ ! -d "$HOME/.marslo/ss" ] && mkdir -p "$HOME/.marslo/ss"
+if [ ! -f "$HOME/.marslo/ss/ssmarslo.json" ]; then
+cat > "$HOME/.marslo/ss/ssmarslo.json" << EOF
 {
     "server":"45.35.34.44",
     "server_port":8838,
@@ -159,8 +159,8 @@ EOF
 function installAptApps() {
   APTSOURCEPATH="/etc/apt/sources.list.d"
   # [ -f /etc/apt/sources.list ] && sudo cp /etc/apt/sources.list{,.org.${TIMESTAMPE}}
-  [ -f ${APTSOURCEPATH}/docker.list ] && sudo mv ${APTSOURCEPATH}/docker.list{,bak.${TIMESTAMPE}}
-  [ -f ${APTSOURCEPATH}/kubernetes.list ] && sudo mv ${APTSOURCEPATH}/kubernetes.list{,bak.${TIMESTAMPE}}
+  [ -f ${APTSOURCEPATH}/docker.list ] && sudo mv "${APTSOURCEPATH}/docker.list{,bak.${TIMESTAMPE}}"
+  [ -f ${APTSOURCEPATH}/kubernetes.list ] && sudo mv "${APTSOURCEPATH}/kubernetes.list{,bak.${TIMESTAMPE}}"
 
   sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6DA746A05F00FA99
   ${CURL} -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
@@ -176,7 +176,7 @@ function installAptApps() {
   sudo ubuntu-drivers autoinstall
 sudo apt install menu debian-keyring g++-multilib g++-7-multilib gcc-7-doc libstdc++6-7-dbg gcc-multilib autoconf automake libtool flex bison gcc-doc gcc-7-multilib gcc-7-locales libgcc1-dbg libgomp1-dbg libitm1-dbg libatomic1-dbg libasan4-dbg liblsan0-dbg libtsan0-dbg libubsan0-dbg libcilkrts5-dbg libmpx2-dbg libquadmath0-dbg glibc-doc:i386 locales:i386 glibc-doc libstdc++-7-doc make-doc libvdpau-va-gl1 nvidia-vdpau-driver nvidia-legacy-340xx-vdpau-drivera -y
   sudo apt install ubuntu-restricted-extras -y
-  sudo apt install -y net-tools bash-completion tree dos2unix iptables-persistent mailutils policycoreutils build-essential gcc g++ make cmake liblxc1 lxc-common lxcfs landscape-common update-motd update-notifier-common apt-file netfilter-persistent ncurses-doc binutils cpp cpp-5 dpkg-dev fakeroot g++-5 gcc gcc-5 libasan2 libatomic1 libc-dev-bin libc6-dev libcc1-0 libcilkrts5 libexpat1-dev libfakeroot libisl15 libitm1 liblsan0 libmpc3 libmpx0 libquadmath0 libstdc++-5-dev libtsan0 libubsan0 linux-libc-dev manpages-dev libssl-dev jq htop dstat ifstat libncurses5-dev libncursesw5-dev libpython-all-dev python-pip binutils-doc cpp-doc gcc-5-locales debian-keyring g++-multilib g++-5-multilib gcc-5-doc libstdc++6-5-dbg gcc-multilib autoconf automake libtool flex bison gdb gcc-doc gcc-5-multilib libgcc1-dbg libgomp1-dbg libitm1-dbg libatomic1-dbg libasan2-dbg liblsan0-dbg libtsan0-dbg libubsan0-dbg libcilkrts5-dbg libmpx0-dbg libquadmath0-dbg libstdc++-5-doc python-setuptools-doc libpython2.7 dlocate python-docutils git m4 ruby texinfo libbz2-dev libexpat-dev libncurses-dev zlib1g-dev iftop libsensors4 sysstat traceroute vim-gtk3 figlet screenfetch dconf-editor m2crypto ctags ntp nautilus-admin libgnome2-bin tmux screen gnome-tweaks gnome-tweak-tool nmap git shadowsocks-qt5 vim-gtk3 xscreensaver xscreensaver-gl-extra xscreensaver-data-extra xscreensaver* tig guake shellcheck
+  sudo apt install -y net-tools bash-completion tree dos2unix iptables-persistent mailutils policycoreutils build-essential gcc g++ make cmake liblxc1 lxc-common lxcfs landscape-common update-motd update-notifier-common apt-file netfilter-persistent ncurses-doc binutils cpp cpp-5 dpkg-dev fakeroot g++-5 gcc gcc-5 libasan2 libatomic1 libc-dev-bin libc6-dev libcc1-0 libcilkrts5 libexpat1-dev libfakeroot libisl15 libitm1 liblsan0 libmpc3 libmpx0 libquadmath0 libstdc++-5-dev libtsan0 libubsan0 linux-libc-dev manpages-dev libssl-dev jq htop dstat ifstat libncurses5-dev libncursesw5-dev libpython-all-dev python-pip binutils-doc cpp-doc gcc-5-locales debian-keyring g++-multilib g++-5-multilib gcc-5-doc libstdc++6-5-dbg gcc-multilib autoconf automake libtool flex bison gdb gcc-doc gcc-5-multilib libgcc1-dbg libgomp1-dbg libitm1-dbg libatomic1-dbg libasan2-dbg liblsan0-dbg libtsan0-dbg libubsan0-dbg libcilkrts5-dbg libmpx0-dbg libquadmath0-dbg libstdc++-5-doc python-setuptools-doc libpython2.7 dlocate python-docutils git m4 ruby texinfo libbz2-dev libexpat-dev libncurses-dev zlib1g-dev iftop libsensors4 sysstat traceroute vim-gtk3 figlet screenfetch dconf-editor m2crypto ctags ntp nautilus-admin libgnome2-bin tmux screen gnome-tweaks gnome-tweak-tool nmap git shadowsocks-qt5 vim-gtk3 xscreensaver xscreensaver-gl-extra xscreensaver-data-extra xscreensaver* tig guake shellcheck dconf-editor
   sudo apt install -y sysstat
   sudo apt install -y gir1.2-gtop-2.0 gir1.2-networkmanager-1.0  gir1.2-clutter-1.0 chrome-gnome-shell
 
@@ -208,7 +208,7 @@ EOF
   [ ! -d "${GITHOME}/marslo" ] && mkdir -p ${GITHOME}/marslo
   [ ! -d "${GITHOME}/tools" ] && mkdir -p ${GITHOME}/tools
   [ ! -d "$HOME/.local/share/gnome-shell/extensions" ] && mkdir -p "$HOME/.local/share/gnome-shell/extensions"
-  [ ! -d $HOME/.marslo ] && mkdir -p $HOME/.marslo
+  [ ! -d "$HOME/.marslo" ] && mkdir -p "$HOME/.marslo"
 
   git clone git@github.com:Marslo/mytools.git ${GITHOME}/marslo/mytools
   git clone git@github.com:Marslo/myvim.git ${GITHOME}/marslo/myvim
@@ -220,13 +220,14 @@ EOF
 }
 
 function devEnv(){
-  cp ${GITHOME}/marslo/myvim/Configurations/vimrc_mac $HOME/.vimrc
-  cp ${GITHOME}/marslo/mylinux/Configs/HOME/Git/.gitconfig $HOME/.gitconfig
-  cp ${GITHOME}/marslo/mylinux/Configs/HOME/.marslo/.marslorc $HOME/.marslo/.marslorc
-  cp ${GITHOME}/marslo/mylinux/Configs/HOME/.marslo/.bello_ubuntu $HOME/.marslo/.bello_ubuntu
+  cp "${GITHOME}/marslo/myvim/Configurations/vimrc_mac" "$HOME/.vimrc"
+  cp "${GITHOME}/marslo/mylinux/Configs/HOME/Git/.gitconfig" "$HOME/.gitconfig"
+  cp "${GITHOME}/marslo/mylinux/Configs/HOME/.marslo/.marslorc" "$HOME/.marslo/.marslorc"
+  cp "${GITHOME}/marslo/mylinux/Configs/HOME/.marslo/.bello_ubuntu" "$HOME/.marslo/.bello_ubuntu"
+  cp "${GITHOME}/marslo/mylinux/Configs/HOME/.marslo/.bye_marslo" "$HOME/.marslo/.bye_marslo"
   echo "source /home/marslo/.marslo/.marslorc" >> ~/.bashrc
 
-  [ -f $HOME/.ssh/config ] && mv $HOME/.ssh/config.org.${TIMESTAMPE}
+  [ -f "$HOME/.ssh/config" ] && mv "$HOME/.ssh/config.org.${TIMESTAMPE}"
 cat >> /etc/bash.bashrc << EOF
 HOST  *
       GSSAPIAuthentication no
@@ -296,7 +297,7 @@ function setupProxy() {
 }
 
 function advacnedSetup() {
-  [ -f /etc/gdm3/custom.conf ] && sudo cp /etc/gdm3/custom.conf{,.bak.${TIMESTAMPE}}
+  [ -f /etc/gdm3/custom.conf ] && sudo cp "/etc/gdm3/custom.conf{,.bak.${TIMESTAMPE}}"
   /bin/sed -r -e 's:^#(WaylandEnable.*false.*$):\1:' -i /etc/gdm3/custom.conf
   /bin/sed -r -e 's:^#.*(AutomaticLoginEnable.*$):\1:' -i /etc/gdm3/custom.conf
   /bin/sed -r -e "s:^#.*(AutomaticLogin[^Enable]*=).*$:\1 $(whoami):" -i /etc/gdm3/custom.conf
