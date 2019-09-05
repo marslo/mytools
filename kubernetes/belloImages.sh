a200be582e7cdc86e5b905eec2ba9abf56ac8bb7
marslo <marslo@marvell.com>
2019-09-05 21:20:54 +0800

    upaste

--- kubernetes/belloImages.sh
+++ kubernetes/belloImages.sh
@@ -4,7 +4,7 @@ privateReg='my.private.com'
 officialReg='k8s.gcr.io'
 k8sVer='v1.15.3'
 
-# $ kubeadm config images pull --config kubeadm-conf.yaml
+kubeadm config images pull --config image.yaml
 
 for pkg in "kube-proxy:${k8sVer}" \
            "kube-apiserver:${k8sVer}" \
@@ -12,6 +12,6 @@ for pkg in "kube-proxy:${k8sVer}" \
            "kube-controller-manager:${k8sVer}" \
            "coredns:1.3.1" \
            "pause:3.1"; do
-  echo "docker tag ${privateReg}/${pkg} ${officialReg}/${pkg}"
-  echo "docker rmi ${privateReg}/${pkg}"
+  docker tag ${privateReg}/${pkg} ${officialReg}/${pkg}
+  docker rmi ${privateReg}/${pkg}
 done
