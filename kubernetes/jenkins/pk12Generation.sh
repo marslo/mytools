c66c1db5a4a93a4795555278c94b4423b49f6330
marslo <marslo.jiao@gmail.com>
2019-09-25 13:16:15 +0800

    updaste

--- kubernetes/jenkins/pk12Generation.sh
+++ kubernetes/jenkins/pk12Generation.sh
@@ -1,5 +1,10 @@
 cat ~/.kube/config | grep certificate-authority-data | awk -F' ' '{print $2}' | base64 -d > ca.crt
 cat ~/.kube/config | grep client-certificate-data | awk -F' ' '{print $2}' | base64 -d > client.crt
 cat ~/.kube/config | grep client-key-data | awk -F' ' '{print $2}' | base64 -d > client.key
-ls -altrh .
+
+ls -Altrh .
 openssl pkcs12 -export -out cert.pfx -inkey client.key -in client.crt -certfile ca.crt
+ls -Altrh .
+
+echo '=====> certification:'
+cat ~/.kube/config | grep certificate-authority-data | awk -F' ' '{print $2}' | base64 -d
