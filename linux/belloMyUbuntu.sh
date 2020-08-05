b1456f1b8f9448311923cc97fad5d41619023ae5
marslo <marslo.jiao@gmail.com>
2020-08-05 17:00:20 +0800

    add cask install keycastr to show pressed key on-screen of macos; and screenkey for Linux; Carnac for windows

--- linux/belloMyUbuntu.sh
+++ linux/belloMyUbuntu.sh
@@ -704,7 +704,8 @@ function aptInstall() {
   otherList="gir1.2-gtop-2.0 \
              gir1.2-networkmanager-1.0  \
              gir1.2-clutter-1.0 \
-             chrome-gnome-shell
+             chrome-gnome-shell \
+             screenkey
   "
 
   # android sdk
--- osx/belloMyOSX.sh
+++ osx/belloMyOSX.sh
@@ -434,7 +434,7 @@ function setupBrewApps(){
   regularlist="wget tmux corkscrew tig ifstat binutils diffutils gawk gnutls gzip less file-formula stow telnet iproute2mac ctags jshon colordiff tree vifm p7zip git mas htop watch jfrog-cli-go youtube-dl etcd mas figlet screenfetch glances bash-completion@2 dos2unix nmap rename renameutils pipenv inetutils hadolint"
   regularheadlist="shellcheck bats jq gradle-completion git-flow"
   gnulist="gnu-sed gnu-tar gnu-which grep ed findutils gnu-indent"
-  casklist="dash iterm2-beta google-chrome-dev vlc licecap jietu tickeys macdown xscreensaver firefox-developer-edition macvim balenaetcher"
+  casklist="dash iterm2-beta google-chrome-dev vlc licecap jietu tickeys macdown xscreensaver firefox-developer-edition macvim balenaetcher keycastr"
   # "growl-fork android-sdk background-music omnigraffle xca manico snip little-snitch imageoptim"
 
   for systempkg in ${systemlist}; do
