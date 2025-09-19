15cac8a91a0cebca27c285a6542802d573e6e8df
marslo <marslo.jiao@gmail.com>
2025-09-19 00:03:37 -0700

    chore(checknic): update the script to highlight the bandwidth if it's 10000Mb/s
    Signed-off-by: marslo <marslo.jiao@gmail.com>

--- linux/checkNIC.sh
+++ linux/checkNIC.sh
@@ -4,7 +4,7 @@
 #     FileName : checkNIC.sh
 #       Author : marslo
 #      Created : 2024-10-21 16:03:41
-#   LastChange : 2025-09-18 02:03:33
+#   LastChange : 2025-09-19 00:01:31
 #=============================================================================
 
 set -euo pipefail
@@ -16,6 +16,9 @@ interface="$(/bin/ip route show default | awk '{print $5}')"
 macaddress="$(/bin/ip link show "${interface}" | sed -rn 's|.*ether ([0-9a-fA-F:]{17}).*$|\1|p' | tr '[a-z]' '[A-Z]')"
 ipaddress=$(/bin/ip a s "${interface}" | sed -rn 's|.*inet ([0-9\.]{7,15})/[0-9]{2} brd.*$|\1|p')
 bandwidth=$(sudo /sbin/ethtool "${interface}"| sed -rn 's|\s*Speed:\s*(.+)$|\1|p')
-echo -e "| \033[1;32m$(hostname)\033[0m | ${bandwidth} | ${interface} | ${ipaddress} | ${macaddress} |"
+case "${bandwidth}" in
+  10000Mb/s ) bandwidth="\033[1;32m${bandwidth}\033[0m" ;;
+esac
+echo -e "| \033[1;36m$(hostname)\033[0m | ${bandwidth} | ${interface} | ${ipaddress} | ${macaddress} |"
 
 # vim:tabstop=2:softtabstop=2:shiftwidth=2:expandtab:filetype=sh
