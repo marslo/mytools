b38ee8cbd7dac9990309d1dd695a551cebf215ef
marslo <marslo.jiao@gmail.com>
2026-09-02 19:20:54 -0700

    build(eza,update.sh,theme): add theme download for eza
    Signed-off-by: marslo <marslo.jiao@gmail.com>

--- opt/eza/update.sh
+++ opt/eza/update.sh
@@ -4,7 +4,7 @@
 #      FileName : update.sh
 #        Author : marslo
 #       Created : 2026-05-14 03:01:06
-#    LastChange : 2026-05-14 03:01:39
+#    LastChange : 2026-09-02 19:20:33
 #         Usage : to install or upgrade eza in Linux/ubuntu
 # =============================================================================
 
@@ -32,12 +32,27 @@ function setup() {
 
   printf "eza %s has been setup to %s/%s\n" "${VERSION}" "${HERE}" "${VERSION}"
   printf 'You can create a symlink to the binary in /usr/local/bin by running:\n'
-  printf "  \$ sudo ln -s %s/%s/eza /usr/local/bin/eza" "${HERE}" "${VERSION}"
+  printf "  \$ sudo ln -s %s/%s/eza /usr/local/bin/eza\n" "${HERE}" "${VERSION}"
+}
+
+function theme() {
+  local _prefix="${HOME}"/.config/eza
+  local _theme_path="${_prefix}"/eza-themes
+  test -d "${_prefix}" || mkdir -p "${_prefix}"
+
+  if test -d "${_theme_path}" && git -C "${_theme_path}" rev-parse --is-inside-work-tree >/dev/null 2>&1; then
+    git -C "${_theme_path}" pull --rebase
+    printf "eza-themes has been updated to the latest version in %s\n" "${_theme_path}"
+  else
+    git clone https://git::@github.com/eza-community/eza-themes.git "${_theme_path}"
+    printf "eza-themes has been cloned to %s\n" "${_theme_path}"
+  fi
 }
 
 function main() {
   cleanup
   setup
+  theme
 }
 
 main "$@"
