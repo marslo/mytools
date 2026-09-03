f1e2cf2280c2b0d2145507f6ddedcfef49c394a6
marslo <marslo.jiao@gmail.com>
2026-09-02 18:28:32 -0700

    build(btop): add install/update script for btop
    Signed-off-by: marslo <marslo.jiao@gmail.com>

--- opt/btop/update.sh
+++ opt/btop/update.sh
@@ -3,20 +3,20 @@
 # =============================================================================
 #      FileName : update.sh
 #        Author : marslo
-#       Created : 2026-04-15 15:08:48
-#    LastChange : 2026-09-02 18:26:48
-#         Usage : to upgrade neovim in Linux/ubuntu
-#                 $ mkdir -p /opt/neovim
-#                 $ cp update.sh /opt/neovim/ && cd /opt/neovim/
+#       Created : 2026-09-02 18:18:01
+#    LastChange : 2026-09-02 18:27:09
+#         Usage : to install/upgrade btop in Linux/ubuntu
+#                 $ mkdir -p /opt/btop
+#                 $ cp update.sh /opt/btop/ && cd /opt/btop/
 #                 $ bash update.sh
 # =============================================================================
 
 set -euo pipefail
 
 declare -r HERE="$( dirname "$( readlink -f "${BASH_SOURCE[0]:-$0}" )" )"
-declare -r REPO_PATH='neovim/neovim'
+declare -r REPO_PATH='aristocratos/btop'
 declare -r VERSION="$(curl -fsSL https://api.github.com/repos/${REPO_PATH}/releases/latest | jq -r .tag_name)"
-declare -r PACKAGE_NAME="nvim-linux-$(uname -m).tar.gz"
+declare -r PACKAGE_NAME="btop-$(uname -m)-unknown-linux-musl.tar.gz"
 declare -r FOLDER_NAME="${PACKAGE_NAME%.tar.gz}"
 
 # shellcheck disable=SC2015
@@ -28,12 +28,12 @@ function cleanup() {
 function setup() {
   curl -fsSL -o "${HERE}/${PACKAGE_NAME}" "https://github.com/${REPO_PATH}/releases/download/${VERSION}/${PACKAGE_NAME}"
   test -d "${HERE}/${VERSION}" || mkdir -p "${HERE}/${VERSION}"
-  tar xf "${HERE}/${PACKAGE_NAME}" -C "${HERE}/${VERSION}/" --strip-components=1
+  tar xf "${HERE}/${PACKAGE_NAME}" -C "${HERE}/${VERSION}/" --strip-components=2
 
   test -L "${HERE}/latest" && unlink "${HERE}/latest"
   ln -sf "${HERE}/${VERSION}" "${HERE}/latest"
 
-  echo ">> SUCCESS: nvim ${VERSION} has been installed to ${HERE}/latest → ${HERE}/${VERSION}"
+  echo ">> SUCCESS: btop ${VERSION} has been installed to ${HERE}/latest → ${HERE}/${VERSION}"
 }
 
 function main() {
